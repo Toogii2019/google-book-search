3273de378a41d38cd8a3337dd4a2750294453fd8
Toogii <dashdava@uw.edu>
2020-11-25 11:18:05 -0800

    Revert back to fe

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,22 +9,7 @@ mongoose.connect(process.env.DBURI || "mongodb://localhost/lessondb", {
 });
 
 module.exports = function (app) {
-
-    app.get("/api/getonlinebooks/:searchStr", (req, res) => {
-        const APIURL = "https://www.googleapis.com/books/v1/volumes?q=";
-        const key = `:keyes&key=${BOOKSAPIKEY}`;
-        const completeUrl = APIURL + req.params.searchStr.split(" ").join("+") + key;
-        axios.get(completeUrl)
-        .then(res => {
-            console.log(res.data.items)
-            res.json(res.data.items)
-        })
-        .catch(err => {
-            console.log(err)
-            res.json(err)
-        }) 
-    })
-
+    
     app.post("/api/save", ({body}, res) => {
         BooksCollection.create(body)
         .then(book => res.json(book))
